4a815f6a7eba52385f0187fe328c3797107a8668
Henrique Alves <halvesdeoliveira07@gmail.com>
2024-01-04 21:34:03 -0300

    Desafio Celular Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia (numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone (numero: "458926", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Twitter");
